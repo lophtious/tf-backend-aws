caad2da81b1cd2f36e95668cd92d98c9c4d07715
Zee Malvo <tatendaz@yahoo.com>
2025-06-08 17:49:17 -0400

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,10 +1,10 @@
-resource "aws_kms_key" "backend_bucket_key" {
+resource "aws_kms_key" "terraform_state" {
   description             = "Key to encrypt S3 backend bucket"
   deletion_window_in_days = 10
   enable_key_rotation     = true
 }
 
-resource "aws_kms_alias" "backend_bucket_key_alias"{
+resource "aws_kms_alias" "terraform_state"{
   name          = "alias/${var.env}-${var.product}-${var.project}-tf-state-${random_string.suffix.id}"
-  target_key_id = aws_kms_key.backend_bucket_key.id
+  target_key_id = aws_kms_key.terraform_state.id
 }
