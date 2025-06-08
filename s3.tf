21a4ac6d33f0594feca2f3c0d46a0ca0f3254f86
Zee Malvo <tatendaz@yahoo.com>
2025-06-08 17:47:47 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "terraform-state" {
+resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.env}-${var.product}-${var.project}-tf-state-${random_string.suffix.id}"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-state" {
-  bucket = aws_s3_bucket.terraform-state.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
 
   rule {
     apply_server_side_encryption_by_default {
