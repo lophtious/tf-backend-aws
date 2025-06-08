3421fa2f903444f3d83e0737a1dbb1dbb336ec0f
Zee Malvo <tatendaz@yahoo.com>
2025-06-08 16:10:21 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,3 @@
-
 resource "aws_s3_bucket" "terraform-state" {
   bucket = "${var.env}-${var.product}-${var.project}-${random_string.suffix.id}"
   force_destroy = true
@@ -11,5 +10,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform-state" 
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"
       kms_master_key_id = aws_kms_key.backend_bucket_key.id
+    }
   }
 }
