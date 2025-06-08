44ba87c5cdde798d633b1f55dc83d7b62732a3bc
Zee Malvo <tatendaz@yahoo.com>
2025-06-08 17:49:43 -0400

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "terraform-state" {
+resource "aws_dynamodb_table" "terraform_state" {
   name = "${var.env}-${var.product}-${var.project}-tf-state-${random_string.suffix.id}"
   hash_key = "LockID"
   read_capacity = 20
