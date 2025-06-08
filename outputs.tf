4650563300176f49e215c9bda4b8a227556fcd3f
Zee Malvo <tatendaz@yahoo.com>
2025-06-08 17:53:48 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "terraform_state_bucket_name" {
-  value = aws_s3_bucket.terraform-state.id
+  value = aws_s3_bucket.terraform_state.id
 }
 
 output "terraform_state_dynamodb_table" {
-  value = aws_dynamodb_table.dynamodb.name
+  value = aws_dynamodb_table.terraform_state.name
 }
